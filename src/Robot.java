265dd3d9b393ec1b71a93082330a07d7a1d807e0
ijiho <jlee120607@gmail.com>
2026-07-03 10:46:26 +0900

    feat: implemented some necessary data structures for A*, wrote getNeighbors() method but still not complete

--- src/Robot.java
+++ src/Robot.java
@@ -1,5 +1,6 @@
 import java.awt.Point;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import static java.lang.Math.pow;
 
@@ -30,11 +31,13 @@ class Node implements Comparable<Node> {
 
 class AStar {
     PriorityQueue<Node> openList = new PriorityQueue<>();
-    int[][] grid;
+    HashSet<Point> closedSet = new HashSet<>();
+    HashMap<Point, Double> gScore = new HashMap<>();
+    char[][] grid;
     Point start;
     Point goal;
 
-    public AStar(int[][] grid, Point start, Point goal) {
+    public AStar(char[][] grid, Point start, Point goal) {
         this.grid = grid;
         this.start = start;
         this.goal = goal;
@@ -45,16 +48,38 @@ class AStar {
         startNode.g = 0;
         startNode.h = calculateDistance(start, goal);
         startNode.f = startNode.g + startNode.h;
+        startNode.position = start;
 
         openList.add(startNode);
+        gScore.put(start, 0.0);
     }
 
     public double calculateDistance(Point a, Point b) {
         return pow(pow((b.x - a.x), 2) + pow((b.y-a.y), 2), 0.5);
     }
 
-    ArrayList<Node> getNeighbors() {
+    void getNeighbors(Node currentNode) {
+        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        for (int[] direction : directions) {
+            int dy = direction[0];
+            int dx = direction[1];
 
+            int newX = currentNode.position.x + dx;
+            int newY = currentNode.position.y + dy;
+            if (0 <= newX && newX < grid[0].length && 0 <= newY && newY < grid.length) {
+                if (closedSet.contains(new Point(newX, newY))){
+                    continue;
+                }
+                if (grid[newY][newX] == ' ') {
+                    Node newNode = new Node(new Point(newX, newY), currentNode);
+                    newNode.g = currentNode.g + 1;
+                    newNode.h = calculateDistance(newNode.position, goal);
+                    newNode.f = newNode.g + newNode.h;
+                    openList.add(newNode);
+
+                }
+            }
+        }
     }
 
 }
